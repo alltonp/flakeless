63ea5684f062e312219fc052c2f881dbaef09c06
pall <paulallton@gmail.com>
2018-06-13 14:59:49 +0100

    further test beefing

--- src/test/scala/im/mange/flakeless/ReportJsonSpec.scala
+++ src/test/scala/im/mange/flakeless/ReportJsonSpec.scala
@@ -1,8 +1,11 @@
 package im.mange.flakeless
 
-import im.mange.flakeless.innards.{Context, DataPoint, FlightDataRecord, FlightDataRecordJson}
+import im.mange.flakeless.innards._
 import org.joda.time.DateTime
+import org.openqa.selenium.By
 import org.scalatest.refspec.RefSpec
+
+import scala.collection.immutable.HashMap
 
 class ReportJsonSpec extends RefSpec {
   def `jsonise FlightDataRecord with no data points`() = {
@@ -28,9 +31,10 @@ class ReportJsonSpec extends RefSpec {
     val record = FlightDataRecord("suite", "test",
       Some(DateTime.parse("2018-06-13T13:25:56.219Z")),
       Some(DateTime.parse("2018-06-13T13:25:58.219Z")),
-      List(DataPoint(0, DateTime.parse("2018-06-13T13:25:57.219Z"), Some("description"),
-        None,
-        Context(), None)))
+      List(
+        DataPoint(0, DateTime.parse("2018-06-13T13:25:57.219Z"), Some("description"), Some(
+            ReportCommand("name", Some("in"), List(By.id("id")), HashMap("key" -> "value"), Some("expected"), Some(List("expectedMany")))),
+          Context(List("failure"), Some(false)), None)))
 
     val expected = """{
                      |  "suite":"suite",
@@ -41,11 +45,25 @@ class ReportJsonSpec extends RefSpec {
                      |    "flightNumber":0,
                      |    "when":"2018-06-13T13:25:57.219Z",
                      |    "description":"description",
+                     |    "command":{
+                     |      "name":"name",
+                     |      "in":"in",
+                     |      "bys":[{
+                     |        "id":"id"
+                     |      }],
+                     |      "args":{
+                     |        "key":"value"
+                     |      },
+                     |      "expected":"expected",
+                     |      "expectedMany":["expectedMany"]
+                     |    },
                      |    "context":{
-                     |      "failures":[]
+                     |      "failures":["failure"],
+                     |      "success":"false"
                      |    }
                      |  }]
-                     |}""".stripMargin
+                     |}
+                     |""".stripMargin
 
     val actual = FlightDataRecordJson.serialise(record)
 
